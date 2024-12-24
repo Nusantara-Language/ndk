3fe94770c80daf514d10a632742b6d6685ab9cb4
Fern-Aerell <fernaerell2020@gmail.com>
2024-12-24 16:35:00 +0700

    update: menuliskan dokumentasi untuk file header cetak dan bisa_di_cetak

--- nstdcpp/include/cetak/cetak.hpp
+++ nstdcpp/include/cetak/cetak.hpp
@@ -1,58 +1,119 @@
+/*
+ * ----------------------------------------------------------------------------
+ * Proyek: Nusantara Language
+ * Penulis: Fern Aerell
+ * Lisensi: BSD 3-Clause License
+ * Hak Cipta (c) 2024, Nusantara
+ * ----------------------------------------------------------------------------
+ */
+
+// Blokir pengulangan header
 #pragma once
 
-#include <format>
+
 #include <iostream>
-#include <type_traits>
+#include <format>
+
+
+#include "cetak/bisa_di_cetak.hpp"
+
 
 namespace nstd {
 
-  // Struktur untuk menentukan apakah tipe T bisa dicetak ke std::cout
-  template <typename T, typename = void>
-  struct CanOutput : std::false_type {};
-
-  // Spesialisasi untuk tipe yang dapat dicetak ke std::cout
-  template <typename T>
-  struct CanOutput<T, std::void_t<decltype(std::cout << std::declval<T>())>> : std::true_type {};
-
-  // Fungsi cetak untuk tipe yang dapat dicetak ke std::cout
+  /**
+   * Fungsi cetak untuk tipe yang dapat dicetak ke std::cout.
+   *
+   * @tparam Type Jenis data yang ingin dicetak.
+   *
+   * @param nilai Nilai yang ingin dicetak.
+   */
   template<typename Type>
   void cetak(const Type& nilai) {
-    if constexpr (CanOutput<Type>()) {
+    // Periksa apakah nilai tersebut dapat dicetak
+    if (BisaDiCetak<Type>()) {
+      // Jika dapat, tuliskan nilainya ke std::cout
       std::cout << nilai;
     } else {
+      // Jika tidak dapat, tampilkan pesan kesal
       std::cerr << "Tipe data " << typeid(Type).name() << " tidak dapat dicetak.\n";
     }
   }
 
-  // Fungsi cetak untuk banyak nilai sekaligus
+  /**
+   * Fungsi cetak untuk banyak nilai sekaligus.
+   *
+   * @tparam Types Jenis-jenis data yang ingin dicetak.
+   * @param kumpulanNilai Kumpulan nilai yang ingin dicetak.
+   */
   template<typename... Types>
   void cetak(const Types&... kumpulanNilai) {
-    (cetak(kumpulanNilai), ...);  // Menggunakan fold expression untuk pemanggilan fungsi cetak secara rekursif
+    /**
+     * Menggunakan fold expression untuk pemanggilan fungsi cetak secara rekursif.
+     * Fungsi ini akan memanggil dirinya sendiri untuk setiap argumen dalam parameter list.
+     */
+    (cetak(kumpulanNilai), ...);
   }
 
-  // Fungsi cetak dengan format string dan argumen tambahan
+  /**
+   * Fungsi cetak dengan format string dan argumen tambahan.
+   *
+   * @tparam Types Jenis-jenis data yang ingin dicetak.
+   * @param formatString Format string yang akan digunakan untuk mencetak nilai.
+   * @param kumpulanNilai Kumpulan nilai yang ingin dicetak.
+   */
   template<typename... Types>
   void cetakDF(const std::format_string<Types...>& formatString, const Types&... kumpulanNilai) {
+    /**
+     * Menggunakan vformat untuk mencetak nilai sesuai dengan format string.
+     */
     cetak(std::vformat(formatString.get(), std::make_format_args(kumpulanNilai...)));
   }
 
-  // Fungsi cetak dengan baris baru (termasuk penambahan "\n")
+  /**
+   * Fungsi cetak dengan baris baru (termasuk penambahan "\n")
+   *
+   * @tparam Type Jenis data yang ingin dicetak.
+   * @param nilai Nilai yang ingin dicetak.
+   */
   template<typename Type>
   void cetakDBB(const Type& nilai) {
+    /**
+     * Fungsi ini memanggil fungsi cetak dengan parameter biasa, kemudian menambahkan baris baru
+     * untuk memisahkan antara hasil cetakan dan teks lain.
+     */
     cetak(nilai);
     std::cout << "\n";
   }
 
-  // Fungsi cetak banyak nilai sekaligus dengan baris baru
+  /**
+   * Fungsi cetak banyak nilai sekaligus dengan baris baru.
+   *
+   * @tparam Types Jenis-jenis data yang ingin dicetak.
+   * @param kumpulanNilai Kumpulan nilai yang ingin dicetak.
+   */
   template<typename... Types>
   void cetakDBB(const Types&... kumpulanNilai) {
-    (cetak(kumpulanNilai), ...);  // Pemanggilan cetak untuk setiap nilai
+    /**
+     * Pemanggilan fungsi cetak untuk setiap nilai dalam parameter list,
+     * kemudian ditambahkan baris baru untuk memisahkan hasil cetakan.
+     */
+    (cetak(kumpulanNilai), ...);
     std::cout << "\n";
   }
 
-  // Fungsi cetak dengan format dan baris baru
+  /**
+   * Fungsi cetak dengan format string dan baris baru.
+   *
+   * @tparam Types Jenis-jenis data yang ingin dicetak.
+   * @param formatString Format string yang akan digunakan untuk mencetak nilai.
+   * @param kumpulanNilai Kumpulan nilai yang ingin dicetak.
+   */
   template<typename... Types>
   void cetakDBBDF(const std::format_string<Types...>& formatString, const Types&... kumpulanNilai) {
+    /**
+     * Fungsi ini memanggil fungsi cetak dengan format string,
+     * kemudian ditambahkan baris baru untuk memisahkan hasil cetakan.
+     */
     cetakDF(formatString, kumpulanNilai...);
     std::cout << "\n";
   }
